b2b4bf3ed1fb72f34dd0ab737d965a692f76bac6
Ellie <dalshot7@naver.com>
2022-12-01 23:07:39 +0900

    feat : reactive navbar

--- component/layout/navbar.tsx
+++ component/layout/navbar.tsx
@@ -3,11 +3,13 @@ import { FaSistrix, FaUser } from "react-icons/fa";
 import Image from "next/image";
 import blueWhale from "../../public/blueWhale.png";
 import { useState, useEffect } from "react";
+import classNames from "classnames";
 
 const Navbar = () => {
   //스크롤 시 navbar 색 변경
   const [color, setColor] = useState(false);
   const [search, setSearch] = useState(false);
+  const [menuToggle, setMenuToggle] = useState(false);
   const changeColor = () => {
     if (10 < window.scrollY) {
       setColor(true);
@@ -32,49 +34,105 @@ const Navbar = () => {
       }
     >
       <div className="relative flex flex-wrap justify-between items-center text-xl ">
-        <div className=" w-full md:block md:w-auto mt-0 ">
-          <ul className=" flex flex-col mx-20 mt-0 md:flex-row md:space-x-8 md:mt-0 md:text-base md:font-xs md:border-0  ">
-            {/*  bg-gray-50 rounded-lg border border-gray-100  md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 */}
-            <Link href="/">
-              <Image
-                className="bg-transparent"
-                src={blueWhale}
-                alt=""
-                width={70}
-                height={20}
-              ></Image>
-            </Link>
-            <li className="hover:font-bold">
-              <Link href="/introduction">회사소개</Link>
-            </li>
-            <li className="hover:font-bold">
-              <Link href="/education">교육</Link>
-            </li>
+        <div className="hidden md:flex items-center space-x-1">
+          <div className=" w-full md:block md:w-auto mt-0 ">
+            <ul className=" flex flex-col mx-20 mt-0 md:flex-row md:space-x-8 md:mt-0 md:text-base md:font-xs md:border-0  ">
+              {/*  bg-gray-50 rounded-lg border border-gray-100  md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 */}
+              <Link href="/">
+                <Image
+                  className="bg-transparent"
+                  src={blueWhale}
+                  alt=""
+                  width={70}
+                  height={20}
+                ></Image>
+              </Link>
+              <li className="hover:font-bold">
+                <Link href="/introduction">회사소개</Link>
+              </li>
+              <li className="hover:font-bold">
+                <Link href="/education">교육</Link>
+              </li>
 
-            <li className="hover:font-bold ">
-              <Link href="/solution">전문 솔루션</Link>
-            </li>
+              <li className="hover:font-bold ">
+                <Link href="/solution">전문 솔루션</Link>
+              </li>
 
-            <li className="hover:font-bold">
-              <Link href="/around">둘러보기</Link>
-            </li>
-            <li className="hover:font-bold">
-              <Link href="/support">고객지원</Link>
-            </li>
-          </ul>
-        </div>
-
-        <div className="flex flex-col mx-20 md:flex-row md:space-x-8 md:mt-0 md:text-xs md:font-medium md:border-0 rounded-lg">
-          {/*  bg-gray-50  border border-gray-100  md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 */}
-
-          <FaSistrix onClick={handleSearchModal} />
-
-          <Link href="/login">
-            <FaUser />
-          </Link>
-          <div className="hover:text-slate-400">
-            <Link href="/store">스토어</Link>
+              <li className="hover:font-bold">
+                <Link href="/around">둘러보기</Link>
+              </li>
+              <li className="hover:font-bold">
+                <Link href="/support">고객지원</Link>
+              </li>
+            </ul>
           </div>
+
+          <div className="flex flex-col mx-20 md:flex-row md:space-x-8 md:mt-0 md:text-xs md:font-medium md:border-0 rounded-lg">
+            {/*  bg-gray-50  border border-gray-100  md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 */}
+
+            <FaSistrix onClick={handleSearchModal} />
+
+            <Link href="/login">
+              <FaUser />
+            </Link>
+            <div className="hover:text-slate-400">
+              <Link href="/store">스토어</Link>
+            </div>
+          </div>
+        </div>
+        {/* mobile menu */}
+        <div className="md:hidden flex items-center">
+          <button onClick={() => setMenuToggle(!menuToggle)}>
+            {menuToggle ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            )}
+          </button>
+        </div>
+        <div className={classNames("md:hidden", { hidden: !menuToggle })}>
+          <Image src={blueWhale} width={100} height={30}></Image>
+          <a href="#" className="block py-2 px-4 text-sm hover:bg-gray-200">
+            회사소개
+          </a>
+          <a href="#" className="block py-2 px-4 text-sm hover:bg-gray-200">
+            교육
+          </a>
+          <a href="#" className="block py-2 px-4 text-sm hover:bg-gray-200">
+            전문솔루션
+          </a>
+          <a href="#" className="block py-2 px-4 text-sm hover:bg-gray-200">
+            둘러보기
+          </a>
+          <a href="#" className="block py-2 px-4 text-sm hover:bg-gray-200">
+            고객지원
+          </a>
         </div>
       </div>
     </nav>
--- pages/solution.tsx
+++ pages/solution.tsx
@@ -1,7 +1,23 @@
 import Navbar from "../component/layout/navbar";
+import yacht4 from "../public/yacht4.jpeg";
+import Image from "next/image";
+import OnImageText from "../component/common/onimagetext";
 
 const Solution = () => {
-  return <Navbar />;
+  return (
+    <div className="place-content-cente justify-items-center mx-auto ">
+      <Navbar />
+      <Image src={yacht4} alt="" width={1600} height={700}></Image>
+      <div className="place-content-center justify-items-center place-items-center mx-auto p-20 items-center justify-center flex px-10">
+        <OnImageText
+          className="relative p-10 place-content-center justify-items-center place-items-center mx-20"
+          smalltext=""
+          name={"공공안전"}
+          subtitle={"항공 인사이트로 효과적인 대응"}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Solution;
