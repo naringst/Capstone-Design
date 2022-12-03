37d5ff51bfdf0a7f552cd5185fc94fd6f764d7b5
Ellie <dalshot7@naver.com>
2022-12-03 22:07:44 +0900

    fix : education page

--- pages/education.tsx
+++ pages/education.tsx
@@ -1,10 +1,16 @@
+import Footer from "../component/layout/footer";
 import Navbar from "../component/layout/navbar";
 
 const Education = () => {
   return (
     <>
       <Navbar />
-      <h1>Education page</h1>
+      <div className="m-20 p-20">
+        <h1 className="text-center font-semibold text-5xl ibm my-2">
+          모두에게 교육받을 기회를 제공합니다
+        </h1>
+      </div>
+      <Footer />
     </>
   );
 };
