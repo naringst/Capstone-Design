19257557cddb08eb3c35f03c59246aba3bc0a338
Ellie <dalshot7@naver.com>
2022-12-04 20:46:40 +0900

    fix : navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Navbar2 from "../component/layout/navbar2";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
