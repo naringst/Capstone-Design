52aab6c434264b2710775edbde7e8ee79199016b
Ellie <dalshot7@naver.com>
2022-12-04 20:47:24 +0900

    feat : arrange litem lists

--- component/common/subItem.tsx
+++ component/common/subItem.tsx
@@ -3,27 +3,27 @@ import drone3 from "../../public/drone3.jpeg";
 
 const SubItem = (props: any) => {
   return (
-    <div className="flex py-3 content-center justify-center">
-      <div className="relative text-center mx-3 ">
+    <div className="md:flex md:py-3 content-center justify-center">
+      <div className="relative text-center md:mx-3 mx-auto  ">
         <Image
           className="rounded-xl justify-center"
           src={props.image1}
           width={458}
           height={523}
         />
-        <h1 className="absolute z-1 inset-x-0 top-10 h-16 text-white smoothappear text-start px-10 mx-10 ibm text-xl font-medium">
+        <h1 className="absolute z-1 inset-x-0 top-10 h-16 text-white smoothappear text-start px-10 md:mx-10 mx-auto ibm md:text-xl sm:text-lg text-lg font-medium">
           {props.name1}
         </h1>
       </div>
 
-      <div className="relative text-center mx-3">
+      <div className="relative text-center md:mx-3 mx-auto ">
         <Image
           className="rounded-xl justify-center"
           src={props.image2}
           width={458}
           height={523}
         />
-        <h1 className="absolute z-1 inset-x-0 top-10 h-16 text-white smoothappear text-start  px-10 mx-10  ibm text-xl font-medium">
+        <h1 className="absolute z-1 inset-x-0 top-10 h-16 text-white smoothappear text-start px-10 md:mx-10 mx-auto ibm md:text-xl sm:text-lg text-lg font-medium">
           {props.name2}
         </h1>
       </div>
