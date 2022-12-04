86cb4ed874600b6dacc439c764502b21d28b25d8
Ellie <dalshot7@naver.com>
2022-12-04 20:47:58 +0900

    feat : reactive education page

--- pages/solution.tsx
+++ pages/solution.tsx
@@ -4,18 +4,19 @@ import korea2 from "../public/korea2.jpg";
 import Image from "next/image";
 
 import Footer from "../component/layout/footer";
+import Navbar2 from "../component/layout/navbar2";
 const Fade = require("react-reveal/Fade");
 
 const Solution = () => {
   return (
-    <div className="bg-gray-50 text-center">
-      <Navbar />
-      <div className="items-center p-20 mx-auto ">
+    <div className=" text-center bg-blue-50">
+      <Navbar2 />
+      <div className="items-center md:px-20 md:py-10 mx-auto  ">
         <Fade>
-          <h1 className="ibm font-semibold text-4xl m-10 text-start">
+          <h1 className="ibm font-semibold md:text-4xl text-2xl md:m-10 m-5 px-5 text-start">
             지금 드론은?
           </h1>
-          <h1 className="ibm font-medium text-xl m-10 text-start">
+          <h1 className="ibm font-medium md:text-xl text-normal md:m-10 m-5 px-5 text-start">
             지도에 커서를 올려보세요
           </h1>
         </Fade>
